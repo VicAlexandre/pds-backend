8efa61e4ecb554923a5b65b1f2eddb4d7cabaab3
Kaio <74631438+kaionabuco@users.noreply.github.com>
2025-09-24 23:08:34 -0300

    feat: handler cors para cadastro de usuário

--- internal/app/api.go
+++ internal/app/api.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 type Application struct {
@@ -31,6 +32,16 @@ func (app *Application) Mount(conn *sql.DB) http.Handler {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
+
+	/* cors handler */
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
 	/* models */
 	userModel := &models.UserModel{
