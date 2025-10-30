31ca5a4c7867fd528aed0207feb11db0beb42161
Kaio <74631438+kaionabuco@users.noreply.github.com>
2025-10-30 01:29:54 -0300

    fix?

--- render-build.sh
+++ render-build.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
 
+# Store the current directory
+ORIGINAL_DIR=$(pwd)
+
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
@@ -11,13 +14,10 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src # Make sure we return to where we were
+  cd $ORIGINAL_DIR # Return to original directory
 else
   echo "...Using Chrome from cache"
 fi
-
-# be sure to add Chromes location to the PATH as part of your Start Command
-# export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
 
 go mod download
 go build -o bin/server ./cmd/api
